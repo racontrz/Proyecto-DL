4a386cbaec12251f3793198b7939c408c1973ab8
unknown <racontrz@gmail.com>
2023-12-23 16:05:45 -0300

    .

--- server/src/db.js
+++ server/src/db.js
@@ -1,39 +1,14 @@
-// const { Pool } = require("pg");
-// require("dotenv").config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
-// const pool = new Pool({
-//   port: process.env.PORT || "3000",
-//   user: process.env.USER || "postgres",
-//   host: process.env.HOST || "localhost",
-//   database: process.env.DATABASE || "drums",
-//   password: process.env.PASSWORD || "16350676",
-//   ORIGIN: process.env.ORIGIN || "http://localhost:5173",
-//   allowExitOnIdle: true
-// });
-
-// module.exports = pool;
-
-const {
-  PG_DATABASE,
-  PG_HOST,
-  PG_PASSWORD,
-  PG_PORT,
-  PG_USER,
-} = require("./config.js");
-
-const pg = require("pg");
-// import pg from "pg";
-
-const pool = new pg.Pool({
-  port: PG_PORT,
-  host: PG_HOST,
-  user: PG_USER,
-  password: PG_PASSWORD,
-  database: PG_DATABASE,
-});
-
-pool.on("connect", () => {
-  console.log("Database connected");
+const pool = new Pool({
+  port: process.env.PORT || "3000",
+  user: process.env.USER || "postgres",
+  host: process.env.HOST || "localhost",
+  database: process.env.DATABASE || "drums",
+  password: process.env.PASSWORD || "16350676",
+  ORIGIN: process.env.ORIGIN || "http://localhost:5173",
+  allowExitOnIdle: true
 });
 
 module.exports = pool;
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 require("dotenv").config();
-const { PG_PORT } = require('./config.js');
+const { PORT } = require('./config.js');
 
 
 
-app.listen(PG_PORT)
-console.log(`Server on port, ${ PG_PORT }`)
+app.listen(PORT)
+console.log(`Server on port, ${ PORT }`)
