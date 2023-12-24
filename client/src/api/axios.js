b7243b3b10fe94d5ee9b54bdaeb4cec141965d11
unknown <racontrz@gmail.com>
2023-12-23 22:53:05 -0300

    .

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BAKEND || "http://localhost:3000/api";
+const baseURL = import.meta.env.VITE_BAKEND || "http://localhost:3000/api";
 
 const client = axios.create({
   baseURL,
